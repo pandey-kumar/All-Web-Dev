28b8ebfce914430e7ed9e3748bd5228a37e58325
shubham pandey <120294693+pandey-kumar@users.noreply.github.com>
2025-01-09 10:47:59 +0000

    changed script

--- JAVASCRIPT/part10_Projects/1_todoList/script.js
+++ JAVASCRIPT/part10_Projects/1_todoList/script.js
@@ -1,17 +1,16 @@
-<<<<<<< HEAD
+
 // I have to do anything whenever the DOM content is loaded:-
 
 document.addEventListener("DOMContentLoaded",()=>{
     // Lets grab every element required
-=======
+
 // Lets grab every element required
->>>>>>> 1bae0f1 (added task in array todolist)
+
 
 const todoInput=document.getElementById("todo-input");
 const addTaskBtn=document.getElementById("add-task-btn");
 const todoList=document.getElementById("todo-list");
 
-<<<<<<< HEAD
 // making an array to store the object values 
 // for first time it will be empty but after wards it should read from local storage
 // in local storage everything is in object format to parse in object
@@ -21,10 +20,8 @@ const tasks= JSON.parse(localStorage.getItem("tasks"))||[];
 // i am also displaying every tasks on the console
 
 tasks.forEach((task)=>renderTask(task));
-=======
-// making an array to store the object values
-const tasks=[];
->>>>>>> 1bae0f1 (added task in array todolist)
+
+
 
 // Addiing Event listener on adding some input and clicking on add Task btn
 
@@ -65,12 +62,13 @@ addTaskBtn.addEventListener("click",()=>{
     
             // Add the new task to the tasks array
             tasks.push(newTask);
-<<<<<<< HEAD
             saveTask();
 
             todoInput.value = ""; // Clear the input box
             console.log(tasks);
-        
+        }
+
+        });
     // Lets make a function to add  tasks to local storage when called everytime
 
     function saveTask(){
@@ -81,16 +79,9 @@ addTaskBtn.addEventListener("click",()=>{
     function renderTask(task){
         console.log(task);
     }
-}
-    });
-    
 
-})
-=======
-    
-            todoInput.value = ""; // Clear the input box
-            console.log(tasks);
-        }
-    });
-    
->>>>>>> 1bae0f1 (added task in array todolist)
+});
+
+
+
+
