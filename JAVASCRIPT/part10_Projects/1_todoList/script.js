d812f04b3a9468d9a769bef3b33da33fd15d04fe
shubham pandey <120294693+pandey-kumar@users.noreply.github.com>
2025-01-10 01:04:48 +0000

    todo completed

--- JAVASCRIPT/part10_Projects/1_todoList/script.js
+++ JAVASCRIPT/part10_Projects/1_todoList/script.js
@@ -15,7 +15,7 @@ const todoList=document.getElementById("todo-list");
 // for first time it will be empty but after wards it should read from local storage
 // in local storage everything is in object format to parse in object
 
-const tasks= JSON.parse(localStorage.getItem("tasks"))||[];
+let tasks= JSON.parse(localStorage.getItem("tasks"))||[];
 
 // i am also displaying every tasks on the console
 
@@ -34,11 +34,13 @@ addTaskBtn.addEventListener("click",()=>{
     // Lets create an object from it and we will push this always in the array
     const newTask={
         id: Date.now(), // providing the random id lets say date as it changes every time
-        task:taskText,
+        text:taskText,
         completed: false  // a default boolean checkmark to manipulate things like is task completed or not
     }
        // lets add this task into the arrray
        tasks.push(newTask);
+       renderTask(newTask);
+       saveTask();
 
        todoInput.value=""; // clearing input box
        console.log(tasks);
@@ -56,12 +58,13 @@ addTaskBtn.addEventListener("click",()=>{
             // Create a new task object
             const newTask = {
                 id: Date.now(), // Unique ID using timestamp
-                task: taskText,
+                text: taskText,
                 completed: false // Default task completion status
             };
     
             // Add the new task to the tasks array
             tasks.push(newTask);
+            renderTask(newTask);
             saveTask();
 
             todoInput.value = ""; // Clear the input box
@@ -77,8 +80,34 @@ addTaskBtn.addEventListener("click",()=>{
 
     // Lets also make a Function to read from the local storage
     function renderTask(task){
-        console.log(task);
+        // console.log(task.text);
+        // here i want to show to user of todo list
+
+        const li=document.createElement("li");
+        li.setAttribute("data-id",task.id);
+        li.innerHTML=`
+        <span>${task.text}</span>
+        <button>Delete</button>
+        `
+
+        li.addEventListener("click",(e)=>{
+            if(e.target.tagName==="BUTTON") return;
+            task.completed=!task.completed;
+            li.classList.toggle("completed");
+            saveTask();
+        });
+
+        li.querySelector("button").addEventListener("click",(e)=>{
+            e.stopPropagation(); // to prevent toggle from  firing 
+
+            // now i will filter out the array of tasks on the basis of clicked
+            tasks=tasks.filter((t)=>t.id !== task.id);
+            li.remove();
+            saveTask();
+        })
+        todoList.appendChild(li);
     }
+
 
 });
 
